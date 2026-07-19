0a1d6751ed32c64dc75ead16dd2223f193fe8362
personal <m.parsa.kolivand@gmail.com>
2026-07-19 17:01:16 +0330

    modify svd module to work with numpy arrays

--- src/svd_compression.py
+++ src/svd_compression.py
@@ -2,8 +2,7 @@ import numpy as np
 import cv2
 
 
-def compress(image, n: int):
-    raw_image = np.array(image)
+def compress(raw_image, n: int):
     R, G, B = cv2.split(raw_image)
 
     U_B, S_B, Vt_B = np.linalg.svd(B, full_matrices=False)
@@ -18,20 +17,3 @@ def compress(image, n: int):
     compressed_image = compressed_image.astype(np.uint8)
     
     return compressed_image
-
-
-def read_file(uploaded_file):
-
-    if uploaded_file is not None:
-        
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-
-        image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-
-    return image
-
-
-if __name__=="__main__":
-    compressed = compress(r"/home/acitate/Projects/LinearAlgebra/picture.jpg", 20)
-    cv2.imwrite(r"/home/acitate/Projects/LinearAlgebra/Compressed.jpg", compressed)
-    
